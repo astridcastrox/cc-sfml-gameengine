cf8d97386c34dcce956038fe25d9927e5c71049c
astridcastrox <castro.astrid.cbtis37@gmail.com>
2021-11-19 15:41:35 -0700

    camera

--- include/InputSystem.hh
+++ include/InputSystem.hh
@@ -1,25 +1,16 @@
 #pragma once
-#include<box2d/box2d.h>
 #include<SFML/Graphics.hpp>
 
-class Rigidbody
+class InputSystem
 {
 private:
-  b2Body* body{};
-  b2BodyDef* bodyDef{};
-  b2Fixture* fixture{};
-  b2FixtureDef* fixtureDef{};
-  b2PolygonShape* polysonShape{};
-  b2World* world{};
-public:
-  Rigidbody(b2World*& world, b2Vec2* position, float width, float height,
-  b2BodyType bodyType, b2Vec2* origin, float angle, float density,
-  float friction, float restitution, void* data);
-  ~Rigidbody();
 
-  b2Body* GetBody() const;
-  b2Vec2 GetPosition() const;
-  sf::Vector2f GetPosition2SFML() const;
-  void MoveBody(b2Vec2 velocity);
-  void FreezeRotation(bool freeze);
+public:
+  InputSystem();
+  ~InputSystem();
+
+  static sf::Vector2f GetAxis();
+  static bool HoverUI(sf::RenderWindow*& window, sf::FloatRect floatRect);
+  static sf::Vector2i GetMousePosition(sf::RenderWindow*& window);
+  static bool OnClick();
 };
